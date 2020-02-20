74458f3db146d01ffff4283d554591c501048365
Krste Nikoloski <Krste.Nikoloski@netcetera.com>
2020-02-20 15:30:47 +0100

    no message

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent } from './components/home/welcome.component';
 
+const routes: Routes = [
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+    { path: '**', pathMatch: 'full', redirectTo: 'welcome' }
+];
+
 @NgModule({
     imports: [
-        RouterModule.forRoot([
-            { path: 'welcome', component: WelcomeComponent },
-            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-            { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-        ])
+        RouterModule.forRoot(routes)
     ],
     exports: [RouterModule]
 })
